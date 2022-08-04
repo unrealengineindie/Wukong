e50947d8a4b5bf29ee87c17852d33b55f260e819
unrealengineindie <unrealengineindie@gmail.com>
2022-08-04 06:31:47 -0400

    Added to git

---
+++ Wukong/Public/Character/NotifyStateRightWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "NotifyStateRightWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WUKONG_API UNotifyStateRightWeapon : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
