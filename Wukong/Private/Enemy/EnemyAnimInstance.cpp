7a8f79d9be0fbe5e2e38443661d0356831842440
unrealengineindie <unrealengineindie@gmail.com>
2022-08-24 08:38:04 -0400

    Created anim instance class for enemy.

--- Wukong/Private/Enemy/EnemyAnimInstance.cpp
+++ Wukong/Private/Enemy/EnemyAnimInstance.cpp
@@ -3,3 +3,27 @@
 
 #include "Enemy/EnemyAnimInstance.h"
 
+#include "Enemy/Enemy.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UEnemyAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	if (Enemy == nullptr)
+	{
+		Enemy = Cast<AEnemy>(TryGetPawnOwner());
+	}
+
+	if (Enemy)
+	{
+		// Get lateral speed of enemy from velocity
+		FVector Velocity{ Enemy->GetVelocity() };
+		Velocity.Z = 0;
+		Speed = Velocity.Size();
+
+		// Get offset yaw to use in blend spaces
+		const FRotator AimRotation = Enemy->GetBaseAimRotation();
+		const FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(Enemy->GetVelocity());
+
+		MovementOffsetYaw = UKismetMathLibrary::NormalizedDeltaRotator(MovementRotation, AimRotation).Yaw;
+	}
+}
