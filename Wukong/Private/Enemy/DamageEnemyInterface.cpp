b34b3aef766e242fafdf4a0c555725c8724236ce
unrealengineindie <unrealengineindie@gmail.com>
2022-08-30 07:20:51 -0400

    Damage enemy interface

---
+++ Wukong/Private/Enemy/DamageEnemyInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy/DamageEnemyInterface.h"
+
+// Add default functionality here for any IDamageEnemyInterface functions that are not pure virtual.
